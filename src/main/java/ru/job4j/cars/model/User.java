555936db2eb03a4e0479e28d9b2333b685f8d4f2
Rubezzz <inigor1@mail.ru>
2024-02-27 23:06:02 +0700

    2. ManyToMany [#331986]

--- src/main/java/ru/job4j/cars/model/User.java
+++ src/main/java/ru/job4j/cars/model/User.java
@@ -3,6 +3,8 @@ package ru.job4j.cars.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "auto_user")
@@ -14,4 +16,12 @@ public class User {
     private int id;
     private String login;
     private String password;
+
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "user_id") },
+            inverseJoinColumns = { @JoinColumn(name = "post_id") }
+    )
+    private List<User> subscription = new ArrayList<>();
 }
